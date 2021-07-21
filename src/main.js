6d0ba5a8e206af8c498d7002834b029ab252c3b0
matheustakasaki <matheus8antunes@gmail.com>
2021-07-20 23:57:53 -0300

    Iniciando desenvolvimento de rotas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
-console.log('oiee');
+import { createServer } from 'http';
+
+
+
+const server = createServer((request, response) => {
+    switch (request.url) {
+        case '/status': {
+            response.writeHead(200, {
+                'Content-Type': 'application/json'
+            });
+            response.write(
+                JSON.stringify({
+                    status : 'okay'
+                })
+            );
+            response.end();
+            return;
+        }
+
+        default: {
+            response.writeHead(404, 'Service not found');
+            response.end();
+        }
+    }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server deu boaa at http://${HOSTNAME}:${PORT}`);
+});
